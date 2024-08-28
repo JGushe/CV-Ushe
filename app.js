d574cdd86f71fa0d672d28aaa98cac32c793fd92
JGushe <159686880+JGushe@users.noreply.github.com>
2024-08-28 00:38:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let python = document.getElementById("python");
+crearBarra(python);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+
+
+let contadores = [-1,-1,-1,-1];
+let entro = false;
+
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 12, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 9, 1, intervalJavascript);
+        },100);
+        const intervalPython = setInterval(function(){
+            pintarBarra(python, 13, 2, intervalPython);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 14, 3, intervalPhotoshop);
+        },100);
+    }
+}
+
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#940253";
+    }else{
+        clearInterval(interval)
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
